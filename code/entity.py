7a43bbffd1f9c4571d3cefdaf46d5e549670fc53
Hamilton Costa <hamiltonvr2@gmail.com>
2025-07-18 11:48:25 -0300

    Adding classes generated from StarUML

---
+++ code/entity.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Entity:
+    def __init__(self):
+        self.name = None
+        self.surf = None
+        self.rect = None
+
+    def name(self, ):
+        pass
+
+    def surf(self, ):
+        pass
+
+    def rect(self, ):
+        pass
+
+    def move(self, ):
+        pass
