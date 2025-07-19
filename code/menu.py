332b01b549a071bdb599f472ba8e08cf14f69866
Hamilton Costa <hamiltonvr2@gmail.com>
2025-07-19 11:12:22 -0300

    Creating assets folder Inserting all assets Creating the main menu: background, sounds and fonts Creating the const file

--- code/menu.py
+++ code/menu.py
@@ -1,9 +1,41 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame as pg
+
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from code.const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_BLACK
+
 
 class Menu:
     def __init__(self, window):
         self.window = window
+        self.surf = pg.image.load('./assets/menuBg.png')
+        self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
-        pass
+        pg.mixer_music.load('./assets/menuSong.wav')
+        pg.mixer_music.set_volume(0.2)
+        pg.mixer_music.play(-1)
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(50, "Mission", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Omega", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
+
+            pg.display.flip()
+
+            # Check for all events
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()  # Close Window
+                    quit()  # End pygame
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pg.font.SysFont(name='Lucida Sans Typewriter', size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
